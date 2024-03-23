2d23be5ddde35a6ecdcf412ddf369bef4c44156c
Vinay Pandya <vinay.pandya@morressier.com>
2024-03-23 10:34:31 +0100

    signin with google

--- App/Pages/Login.js
+++ App/Pages/Login.js
@@ -1,9 +1,53 @@
+//
+// import libraries
 import React from "react";
-import { View, Text, Image, Dimensions, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  Dimensions,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from "@react-native-google-signin/google-signin";
+
+//
+// own component
 import Colors from "../Shared/Colors";
 
 export default function Login() {
+  GoogleSignin.configure({
+    // scopes: ["https://www.googleapis.com/auth/drive.readonly"], // what API you want to access on behalf of the user, default is email and profile
+    iosClientId:
+      "1015804469648-42je1ro7uvubppdlt8j62df48lpnojss.apps.googleusercontent.com", // [iOS] if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+    // googleServicePlistPath: "", // [iOS] if you renamed your GoogleService-Info file, new name here, e.g. GoogleService-Info-Staging
+    // openIdRealm: "", // [iOS] The OpenID2 realm of the home web server. This allows Google to include the user's OpenID Identifier in the OpenID Connect ID token.
+    profileImageSize: 120, // [iOS] The desired height (and width) of the profile image. Defaults to 120px
+  });
+  const signIn = async () => {
+    alert("working");
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      setState({ userInfo });
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+    }
+  };
+
   return (
     <View>
       <Image
@@ -13,15 +57,18 @@ export default function Login() {
       <View style={styles.container}>
         <Text style={styles.title}>Welcome to Education App</Text>
         <Text style={styles.loginTitle}>Login/Signup</Text>
-        <View style={styles.loginButton}>
+        <TouchableOpacity onPress={signIn} style={styles.loginButton}>
           <FontAwesome name="google" size={24} color={Colors.white} />
           <Text style={{ color: Colors.white }}>Sign In with Google</Text>
-        </View>
+        </TouchableOpacity>
       </View>
     </View>
   );
 }
 
+/**
+ * styles
+ */
 const styles = StyleSheet.create({
   container: {
     paddingTop: 40,
