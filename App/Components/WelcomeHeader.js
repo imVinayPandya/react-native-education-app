c95e6c8c02ce5e8c2034d2093cf4981bec3dc8ee
Vinay Pandya <vinay.pandya@morressier.com>
2024-03-23 14:22:36 +0100

    Design header component

---
+++ App/Components/WelcomeHeader.js
@@ -0,0 +1,41 @@
+import { View, Text, StyleSheet, Image } from "react-native";
+import React, { useContext } from "react";
+
+import { AuthContext } from "../Context/AuthContext";
+
+export default function WelcomeHeader() {
+  const { userDetails } = useContext(AuthContext);
+
+  return (
+    <View style={styles.header}>
+      <View>
+        <Text>Hello,</Text>
+        <Text style={styles.userName}>{userDetails.user.name}</Text>
+      </View>
+      <Image
+        source={{ uri: userDetails?.user?.photo }}
+        style={styles.profilePicture}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 16,
+    marginTop: 24,
+  },
+  userName: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  profilePicture: {
+    width: 50,
+    height: 50,
+    borderRadius: 100,
+  },
+});
--- App/Pages/Home.js
+++ App/Pages/Home.js
@@ -1,23 +1,21 @@
 import { View, Text, StyleSheet, Button } from "react-native";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Services from "../Shared/Services";
 import Loading from "../Components/Loading";
+import { AuthContext } from "../Context/AuthContext";
+import WelcomeHeader from "../Components/WelcomeHeader";
 
 export default function Home() {
-  const [userDetails, setUserDetails] = useState();
-
-  useEffect(() => {
-    Services.getUserDetails().then((data) => {
-      setUserDetails(data ?? null);
-    });
-  }, []);
+  const { userDetails, setUserDetails } = useContext(AuthContext);
 
   if (userDetails === undefined) return <Loading />;
 
   return (
-    <View style={styles.container}>
-      <Text>Welcome {userDetails.user.name}</Text>
-      <Button title="Logout" onPress={Services.logoutUser} />
+    <View>
+      <WelcomeHeader />
+      <View style={styles.container}>
+        <Button title="Logout" onPress={Services.logoutUser} />
+      </View>
     </View>
   );
 }
